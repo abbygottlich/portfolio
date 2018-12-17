b04fb41c2559cf4a3d9bcd05fa064b6589af2140
Abby Gottlich <abby.gottlich@gmail.com>
2018-12-16 19:00:22 -0600

    about page functioning but not styled

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-const About = () => {
-  return (
-    <div>
-      <p class="title">About</p>
-    </div>
-  );
-};
+class About extends Component {
+  state = {
+    showMoreInfo: false
+  };
+
+  handleClick = () => {
+    this.setState({
+      showMoreInfo: !this.state.showMoreInfo
+    });
+  };
+
+  render() {
+    const moreInfo = <p>fjergkjhegkjhwgwhgkjwehgkjwgkjwgkj</p>;
+
+    return (
+      <div>
+        <button onClick={this.handleClick}>
+          {this.state.showMoreInfo ? "Read less" : "Read more"}
+        </button>
+        <p class="title">Hi, my name is Abby.</p>
+        {this.state.showMoreInfo && moreInfo}
+      </div>
+    );
+  }
+}
 
 export default About;
--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <p class="title">Home</p>
+      <h1>Welcome to my portfolio.</h1>
     </div>
   );
 };
